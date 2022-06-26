928f650c4d49ed6aea5dff07cbadac6e21b8ca05
Eduards Smulko <Eduardik1915@gmail.com>
2022-06-26 09:57:28 +0300

    Created map inilization with animals. Partially completed with move() method, but at the moment get issue with deleting animal from set.

---
+++ src/main/java/com/javarush/island/smulko/entity/organizms/animals/herbivores/Deer.java
@@ -0,0 +1,10 @@
+package com.javarush.island.smulko.entity.organizms.animals.herbivores;
+
+import com.javarush.island.smulko.entity.preferences.Fields;
+
+public class Deer extends Herbivore{
+
+    public Deer(Fields fields) {
+        super(fields);
+    }
+}
